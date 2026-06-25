83ace6e33f87fe60226cb813796f732f199e9b9c
hananya-ruderman <chruderman@gmail.com>
2026-06-25 14:39:44 +0300

    moved varriables and fallbacks to config file

--- backend/config/env.ts
+++ backend/config/env.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  serverPort: process.env.SERVER_PORT,
+  serverPort: +(process.env.SERVER_PORT || 3000),
   jwtSecret: process.env.JWT_SECRET,
   saltRounds: +(process.env.SALT_ROUNDS?? 10),
+  databaseUrl: process.env.DATABASE_URL
 
 };
