0d75a8c803abddbdf63c14a9858105ae83dd07df
hananya-ruderman <chruderman@gmail.com>
2026-07-30 14:47:46 +0300

    add zod library for validate env config file

--- backend/config/env.ts
+++ backend/config/env.ts
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
+import { z } from "zod";
+
 dotenv.config();
 
-export const env = {
-  serverPort: +(process.env.SERVER_PORT || 3000),
-  jwtSecret: process.env.JWT_SECRET,
-  saltRounds: +(process.env.SALT_ROUNDS?? 10),
-  databaseUrl: process.env.DATABASE_URL
+const envSchema = z.object({
+  SERVER_PORT: z.coerce.number().default(3000),
+  JWT_SECRET: z.string().min(1),
+  SALT_ROUNDS: z.coerce.number().default(10),
+  DATABASE_URL: z.string().min(1),
+});
 
+const parsedEnv = envSchema.parse(process.env);
+
+export const env = {
+  serverPort: parsedEnv.SERVER_PORT,
+  jwtSecret: parsedEnv.JWT_SECRET,
+  saltRounds: parsedEnv.SALT_ROUNDS,
+  databaseUrl: parsedEnv.DATABASE_URL,
 };
